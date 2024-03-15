de16b6e656233b20d81de7d105b87bc75a6e190d
Frans Paasonen <franspaasonen@gmail.com>
2024-03-15 23:59:25 +0200

    feat: Add command for using backups during a paused game state

--- src/states/Live.cs
+++ src/states/Live.cs
@@ -1,6 +1,7 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Cvars;
+using CounterStrikeSharp.API.Modules.Menu;
 using CounterStrikeSharp.API.Modules.Utils;
 
 namespace MatchUp;
@@ -11,10 +12,14 @@ public class LiveState : BaseState
     private bool team1Pause = false;
     private bool team2Pause = false;
 
+    private ConVar? lastBackupConVar = null;
+    private string? lastRoundPlayedBackupFile = null;
+
     public LiveState() : base()
     {
         commandActions["pause"] = (userid, args) => OnPlayerPause(userid);
         commandActions["unpause"] = (userid, args) => OnPlayerUnpause(userid);
+        commandActions["backup"] = (userid, args) => OnPlayerBackup(userid);
 
         // Used for testing
         commandActions["kill"] = (userid, args) => OnPlayerSuicide(userid);
@@ -25,10 +30,13 @@ public class LiveState : BaseState
     {
         Console.WriteLine("Switched to Live state");
 
+
         Console.WriteLine("Executing Live cfg");
         Server.ExecuteCommand("exec MatchUp/live.cfg");
 
         Server.ExecuteCommand("mp_restartgame 3");
+
+        lastBackupConVar = ConVar.Find("mp_backup_round_file_last");
 
         Utils.DelayedCall(TimeSpan.FromSeconds(4), () =>
         {
@@ -42,6 +50,32 @@ public class LiveState : BaseState
     {
         team1Pause = false;
         team2Pause = false;
+        lastRoundPlayedBackupFile = null;
+        lastBackupConVar = null;
+    }
+
+    public override void OnMatchEnd(EventCsWinPanelMatch @event)
+    {
+
+        var delay = 15;
+        var tvEnableCVar = ConVar.Find("tv_enable");
+        if (tvEnableCVar != null && tvEnableCVar.GetPrimitiveValue<bool>())
+        {
+            // TODO: Test which delay should be used here, tv_delay or tv_delay1
+            var tvDelayCvar = ConVar.Find("tv_delay");
+            if (tvDelayCvar != null)
+            {
+                delay += tvDelayCvar.GetPrimitiveValue<int>();
+            }
+        }
+
+        Console.WriteLine($"Waiting for match end panel and cstv delay {delay}");
+
+        Utils.DelayedCall(TimeSpan.FromSeconds(delay), () =>
+        {
+            StateMachine.SwitchState(GameState.Loading);
+            Server.ExecuteCommand($"changelevel {Server.MapName}");
+        });
     }
 
     private void OnPlayerPause(int userid)
@@ -99,34 +133,67 @@ public class LiveState : BaseState
             }
         }
 
+
         if (!team1Pause && !team2Pause)
         {
             Server.ExecuteCommand("mp_unpause_match");
         }
     }
 
-    public override void OnMatchEnd(EventCsWinPanelMatch @event)
+    private void OnPlayerBackup(int userid)
     {
+        var player = Utilities.GetPlayerFromUserid(userid);
 
-        var delay = 15;
-        var tvEnableCVar = ConVar.Find("tv_enable");
-        if (tvEnableCVar != null && tvEnableCVar.GetPrimitiveValue<bool>())
+        var paused = team1Pause || team2Pause;
+        if (paused)
         {
-            // TODO: Test which delay should be used here, tv_delay or tv_delay1
-            var tvDelayCvar = ConVar.Find("tv_delay");
-            if (tvDelayCvar != null)
+            var backupFileName = lastRoundPlayedBackupFile;
+            if (lastBackupConVar != null && lastBackupConVar.StringValue != null && lastBackupConVar.StringValue != "")
             {
-                delay += tvDelayCvar.GetPrimitiveValue<int>();
+                backupFileName = lastBackupConVar.StringValue;
+                lastRoundPlayedBackupFile = lastBackupConVar.StringValue;
+            }
+
+            if (backupFileName != null)
+            {
+                Action<CCSPlayerController, ChatMenuOption> backupHandle =
+                    (CCSPlayerController player, ChatMenuOption option) =>
+                    OnPlayerSelectBackup(player, option.Text);
+
+                var backupSelection = new ChatMenu("Backup selection");
+
+                var lastRound = int.Parse(string.Concat(backupFileName.SkipLast(4).TakeLast(2)));
+                player.PrintToChat($"Last backup file {lastRound}");
+
+                for (var i = 0; i <= lastRound; i++)
+                {
+                    var roundNumberString = i < 10 ? $"0{i}" : $"{i}";
+                    backupSelection.AddMenuOption($"round {roundNumberString}", backupHandle);
+                }
+
+                MenuManager.OpenChatMenu(player, backupSelection);
+            }
+            else
+            {
+                player.PrintToChat($" {ChatColors.Red} Could not load backup data");
             }
         }
-
-        Console.WriteLine($"Waiting for match end panel and cstv delay {delay}");
-
-        Utils.DelayedCall(TimeSpan.FromSeconds(delay), () =>
+        else
         {
-            StateMachine.SwitchState(GameState.Loading);
-            Server.ExecuteCommand($"changelevel {Server.MapName}");
-        });
+            player.PrintToChat($" {ChatColors.Red} Game needs to be paused to use backups");
+        }
+    }
+
+    private void OnPlayerSelectBackup(CCSPlayerController player, string selection)
+    {
+        if (lastRoundPlayedBackupFile != null)
+        {
+            var round = selection.Split(" ")[1];
+            var backupFileName = string.Concat(lastRoundPlayedBackupFile.SkipLast(6)) + $"{round}.txt";
+            player.PrintToChat($"Restoring previous state using backup file {backupFileName}");
+
+            Server.ExecuteCommand($"mp_backup_restore_load_file {backupFileName}");
+        }
     }
 
     // Used for testing
