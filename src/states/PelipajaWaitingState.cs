0879b8a1c704b47fec435296b5b4fe01a28e202f
Tomppahh <Tommi.Paakkunainen@student.lut.fi>
2026-03-20 01:50:58 +0200

    added auto team assignment

--- src/states/PelipajaWaitingState.cs
+++ src/states/PelipajaWaitingState.cs
@@ -1,3 +1,6 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+
 namespace MatchUp.states;
 
 public class PelipajaWaitingState : BaseState
@@ -7,8 +10,24 @@ public class PelipajaWaitingState : BaseState
         Console.WriteLine("[Pelipaja] Waiting for config from Next.js...");
     }
 
-    public override void Leave()
+    public override void Leave() { }
+
+    public override void OnPlayerConnect(EventPlayerConnectFull @event)
     {
-        
+        var player = @event.Userid;
+        if (player == null || !player.IsValid) return;
+
+        var steamId = player.SteamID.ToString();
+
+        if (PelipajaConfig.Team1?.Players.Contains(steamId) == true)
+        {
+            player.ChangeTeam(CsTeam.Terrorist);
+            player.PrintToChat($" {ChatColors.Green}You have been assigned to {PelipajaConfig.Team1.Name}");
+        }
+        else if (PelipajaConfig.Team2?.Players.Contains(steamId) == true)
+        {
+            player.ChangeTeam(CsTeam.CounterTerrorist);
+            player.PrintToChat($" {ChatColors.Green}You have been assigned to {PelipajaConfig.Team2.Name}");
+        }
     }
 }
