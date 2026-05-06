8edd287774427f3c9209400c69f38c0317a1f663
Tomppahh <Tommi.Paakkunainen@student.lut.fi>
2026-05-06 08:02:12 +0300

    update plugin and build script

--- scripts/create-build.sh
+++ scripts/create-build.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env bash
-
-METAMOD_VERSION=1389
 CSSAPI_VERSION=$(dotnet list package --format json | jq -r '.projects[0].frameworks.[0].topLevelPackages.[] | select(.id == "CounterStrikeSharp.API") | .requestedVersion')
+
+# Dynamically fetch the latest MetaMod 2.0 build number
+METAMOD_VERSION=$(curl -s https://mms.alliedmods.net/mmsdrop/2.0/ \
+  | grep -oP 'mmsource-2\.0\.0-git\K[0-9]+(?=-linux)' \
+  | sort -n | tail -1)
+
+echo "Using MetaMod version: $METAMOD_VERSION"
+echo "Using CounterStrikeSharp version: $CSSAPI_VERSION"
 
 mkdir -p build
 cd build
-
 wget -q https://github.com/roflmuffin/CounterStrikeSharp/releases/download/v${CSSAPI_VERSION}/counterstrikesharp-with-runtime-linux-${CSSAPI_VERSION}.zip
 unzip counterstrikesharp-with-runtime-linux-${CSSAPI_VERSION}.zip
 rm counterstrikesharp-with-runtime-linux-${CSSAPI_VERSION}.zip
-
 wget -c https://mms.alliedmods.net/mmsdrop/2.0/mmsource-2.0.0-git${METAMOD_VERSION}-linux.tar.gz -O - | tar -xz
-
 cd ..
-
 dotnet build -c Release
-
 mkdir -p build/addons/counterstrikesharp/plugins/MatchUp
 mkdir -p build/cfg
-
 cp bin/Release/net8.0/* build/addons/counterstrikesharp/plugins/MatchUp/
-
 cp -R cfg/* build/cfg/
-
-
 pushd build
 zip -r MatchUp+cssharp+metamod.zip *
 popd
